0a82399c4e77d728bfdb70d01e21eb7b1f778447
Craig Dennis <craig@teamtreehouse.com>
2017-02-16 17:10:20 -0800

    Composes the markdown into the sanitization

--- src/main/java/com/teamtreehouse/composure/App.java
+++ src/main/java/com/teamtreehouse/composure/App.java
@@ -1,6 +1,7 @@
 package com.teamtreehouse.composure;
 
 import com.teamtreehouse.composure.filters.Html;
+import com.teamtreehouse.composure.filters.Markdown;
 import com.teamtreehouse.composure.utils.Renderer;
 import java.util.function.Function;
 
@@ -9,12 +10,15 @@ public class App {
   public static void main(String[] args) {
     String htmlInput = "<p>Using strong tags <strong>allows you to over promote</strong> and " +
             "<a href=\"http://www.myspace.com/tom\">Links should open in new browser</a></p>";
+    String markdownInput = "Using strong tags **allows you to over promote** and "
+        + "[Links open in a new browser](http://www.google.com)";
 
     Function<String, String> boldCleaner = Html::boldCleaner;
 
     Function<String, String> sanitizer = boldCleaner.andThen(Html::externalizeLinks);
 
-    String result = Renderer.render(htmlInput, sanitizer);
+    Function<String, String> markeddownSanitizer = sanitizer.compose(Markdown::render);
+    String result = Renderer.render(htmlInput, markeddownSanitizer);
 
     System.out.println(result);
   }
