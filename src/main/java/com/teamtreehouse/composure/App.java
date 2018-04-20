60c22feabfb46eccc2bca12c1cd62fa90b687b7c
dragon <1513>
2018-04-20 23:39:49 +0800

    test

--- src/main/java/com/teamtreehouse/composure/App.java
+++ src/main/java/com/teamtreehouse/composure/App.java
@@ -1,15 +1,22 @@
 package com.teamtreehouse.composure;
 
-import com.teamtreehouse.composure.filters.Html;
-
 public class App {
 
   public static void main(String[] args) {
     String htmlInput = "<p>Using strong tags <strong>allows you to over promote</strong> and " +
-            "<a href=\"http://www.myspace.com/tom\">Links should open in new browser</a></p>";
+            "<a href=\"http://www.myspace.com/tom\">Links <strong>should</strong> open in new browser</a></p>";
 
-    String result = Html.boldCleaner(htmlInput);
-
-    System.out.println(result);
+//   String result = Html.boldCleaner(htmlInput);
+//
+//    System.out.println(result);
+// String result = Html.boldCleaner(htmlInput);
+//
+//    System.out.println(result);
+//    String result = Renderer.render(htmlInput, markeddownSanitizer);
+//    System.out.println("HTML is safe...");
+//    System.out.println(result);
+//    result = Renderer.render(markdownInput, markeddownSanitizer);
+//    System.out.println("Markdown is too...");
+//    System.out.println(result);
   }
 }
